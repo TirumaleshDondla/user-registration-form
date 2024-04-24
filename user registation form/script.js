dbe3caecef9ee1989ce6c20349b8511b4b1d19ee
TirumaleshDondla <155290869+TirumaleshDondla@users.noreply.github.com>
2024-04-24 13:04:23 +0530

    Add files via upload

---
+++ user registation form/script.js
@@ -0,0 +1,14 @@
+function registerUser() {
+    //Get values from the form
+
+var name = document.getElementById('name').value;
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+
+//print data to the console
+console.log('name: ' + name);
+console.log('Email: ' + email);
+console.log('Password: ' + password);
+
+//you can add future processing or send the data to the server here
+}
